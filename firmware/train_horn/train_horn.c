a5a46fa7b2d228d1ca3a9870cad725ab1a504791
sdp8483 <leaumasyerrp@gmail.com>
2018-02-25 17:01:44 -0500

    update low note frequency to 256Hz change io to #defines and cleaned up comments

--- firmware/train_horn/train_horn.c
+++ firmware/train_horn/train_horn.c
@@ -9,27 +9,32 @@
 //      could not get MSP430 internal comparator to work
 //
 //  ACLK = TBCLK = 32768Hz, MCLK = SMCLK = default DCODIV ~1MHz
-//  P1.5 = CCR0 ~ 32KHz/(2*66) = ~248.24Hz -> target frequency: 246.94Hz (B3)
-//  P2.0 = CCR1 ~ 32KHz/(2*53) = ~309.13Hz -> target frequency: 311.13Hz (D#4)
-//  P2.1 = CCR2 ~ 32KHz/(2*37) = ~442.81Hz -> target frequency: 440.00Hz (A4)
+//  P1.5 = CCR0 ~ 32KHz/(2*64) = ~256.00Hz -> target frequency: 255Hz (~C4)
+//  P2.0 = CCR1 ~ 32KHz/(2*53) = ~309.13Hz -> target frequency: 311Hz (~D#4)
+//  P2.1 = CCR2 ~ 32KHz/(2*37) = ~442.81Hz -> target frequency: 440Hz (A4)
 //
 //                MSP430FR2000
 //             -----------------
-//         /|\|             P1.0|--> LED for timer status
-//          | |             P1.3|<-- Play Button to gnd
-//          | |             P1.2|--> Audio Amp Shutdown
-//          | |       P1.7/VREF+|--> 1.2VREF Output
-//          | |             P1.5|--> ~248.24Hz
-//          --|RST    P2.0/TB0.1|--> ~309.13Hz
+//            |             P1.0|--> LED for timer status
+//            |             P1.2|--> Audio Amp Shutdown
+//            |             P1.3|<-- Play Button to gnd
+//            |                 |
+//            |       P1.7/VREF+|--> 1.2VREF Output
+//            |                 |
+//            |             P1.5|--> ~248.24Hz
+//            |       P2.0/TB0.1|--> ~309.13Hz
 //            |       P2.1/TB0.2|--> ~442.81Hz
 //            |                 |
 //
 //
-//   TimerB multifrequency based on TI app note SLAA513A and accompanying code
+//   TimerB multi frequency based on TI app note SLAA513A and accompanying code
 //******************************************************************************
 #include <msp430.h>
 
+// Port1 Pin Defines
 #define PLAY_BTN    BIT3                        // Play button port 1 input
+#define PLAY_LED    BIT0                        // Play LED port 1 output
+#define AMP_EN      BIT2                        // Amplifier enable port 1 output
 
 int main(void)
 {
@@ -44,22 +49,30 @@ int main(void)
     P1OUT  |= PLAY_BTN;
 
     // Configure Audio Amp Shutdown
-    P1DIR |= BIT2;                              // P1.2 control shutdown of audio amp
-    P1OUT &= ~BIT2;                             // Pull low to turn off audio amp
+    P1DIR |= AMP_EN;                            // set as output
+    P1OUT &= ~AMP_EN;                           // Pull low to turn off audio amp
+
+    // Configure Play LED indicator
+    P1DIR  |= PLAY_LED;                         // set as output
+    P1OUT  &= ~PLAY_LED;                        // turn off LED
 
     // Configure Timer_B Outputs
     P2SEL0 |= BIT0 | BIT1;                      // Select TB0.1 and TB0.2 pin functions for P2.0 and P2.1
-    P2DIR |= BIT0 | BIT1;
-    P1DIR |= BIT0 | BIT5;                       // P1.0 and P1.5 are outputs
-    P1OUT  &= ~BIT0;                            // turn off LED on P1.0
+    P2DIR  |= BIT0 | BIT1;
+    P1DIR  |= BIT5;                             // P1.5 is an output
+
+    // Configure Timer_B Outputs
+    P2SEL0 |= BIT0 | BIT1;                      // Select TB0.1 and TB0.2 pin functions for P2.0 and P2.1
+    P2DIR  |= BIT0 | BIT1;
+    P1DIR  |= BIT5;                             // P1.5 is an output
 
     // Configure Voltage Reference Output
     P1SEL0 |= BIT7;                             // Select VREF+ pin function for P1.7
     P1SEL1 |= BIT7;
 
-    // Configure Unused GPIO
-    //P1DIR |= BIT3 | BIT4 | BIT6;
-    //P2DIR |= BIT6 | BIT7;
+    // Configure Unused GPIO as outputs per ULP advice
+    P1DIR |= BIT1 | BIT4 | BIT6;
+    P2DIR |= BIT6 | BIT7;
 
     PM5CTL0 &= ~LOCKLPM5;                       // Disable the GPIO power-on default high-impedance mode to activate
                                                 // previously configured port settings
@@ -69,8 +82,8 @@ int main(void)
     PMMCTL2 |= EXTREFEN;                        // Enable external reference
     while(!(PMMCTL2 & REFGENRDY));              // Poll until internal reference settles
 
-    P1IFG &= ~PLAY_BTN;                                  // clear any pending interrupts
-    P1IE |= PLAY_BTN;                                // enable interrupts on P1.1
+    P1IFG &= ~PLAY_BTN;                         // clear any pending interrupts
+    P1IE  |= PLAY_BTN;                          // enable interrupts on P1.1
 
     for(;;) {
         __bis_SR_register(LPM3_bits | GIE);     // Enter LPM3, enable interrupts
@@ -82,9 +95,9 @@ int main(void)
 #pragma vector=PORT1_VECTOR
 __interrupt void PORT1_ISR(void)
 {
-    P1IE &= ~PLAY_BTN;                                   // disable any further interrupts
-    P1OUT ^= BIT0 | BIT2;                       // toggle P1.0 LED for timer_b status
-                                                // toggle P1.2 to turn audio amp on
+    P1IE &= ~PLAY_BTN;                          // disable any further interrupts
+    P1OUT ^= PLAY_LED | AMP_EN;                 // toggle play LED for status indicator
+                                                // toggle audio amp on
 
     // setup timerB
     TB0CCTL0 = OUTMOD_4 + CCIE;                 // TBCCR0 toggle, interrupt enabled
@@ -98,8 +111,8 @@ __interrupt void PORT1_ISR(void)
 __interrupt void TIMER0_B0_ISR(void)
 {
 
-    TBCCR0 += 66;
-    P1OUT ^= BIT5;                              // ~248.24Hz frequency generation on P1.5
+    TBCCR0 += 64;
+    P1OUT ^= BIT5;                              // ~256Hz frequency generation on P1.5
 }
 
 // Timer0_B3 Interrupt Vector (TBIV) handler
@@ -114,11 +127,11 @@ __interrupt void TIMER0_B1_ISR(void)
             TBCCR1 += 53;                       // ~309.13Hz frequency generation on P1.6
             break;
         case TB0IV_TBCCR2:                      // CCR2
-            TBCCR2 += 37;                       // ~442.81Hz freguency generation on P1.7
+            TBCCR2 += 37;                       // ~442.81Hz frequency generation on P1.7
             break;
         case TB0IV_TBIFG:                       // overflow
-            P1OUT    ^= BIT0 | BIT2;            // toggle P1.0 LED for timer_b status
-                                                // turn off audio amp on P1.2
+            P1OUT    ^= PLAY_LED | AMP_EN;      // toggle LED for play status
+                                                // turn off audio amp
             TB0CTL   &= ~(MC_2);                // disable Timer_B
             TB0CCTL0 = 0;                       // disable CCR0, CCR1, CCR2
             TB0CCTL1 = 0;
